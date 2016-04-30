798e33c0887e6276222f9c8ed803a9413bda2744
Israel Zúñiga de la Mora <israelzdlm@gmail.com>
2016-04-30 16:32:56 -0500

    Added source file for API key

--- src/responder.py
+++ src/responder.py
@@ -4,3 +4,9 @@
 import os
 import logging
 import telebot
+
+# Setup the logger
+telebot.logger.setLevel(loggin.DEBUG)
+
+# Load API's key from environment
+responder_bot = telebot.AsyncTeleBot(os.environ['BOT_KEY'])
